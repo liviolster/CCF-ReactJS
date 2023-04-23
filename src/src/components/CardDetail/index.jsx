ea7d09250876815059e82ad6da23391d5de60e91
Liv Iolster <117924322+liviolster@users.noreply.github.com>
2023-04-23 12:07:13 -0300

    Add files via upload

---
+++ src/src/components/CardDetail/index.jsx
@@ -0,0 +1,66 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Navigate, useParams, Link } from "react-router-dom";
+import db from "../../../db/firebase-config";
+import styles from "./cardDetail.module.css";
+import { useCartContext } from "../../context/CartContext"
+import ItemCount from "../../ItemCount";
+
+
+const CardDetail = () => {
+  const [producto, setProducto] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+const {addProduct} = useCartContext();
+
+const [goToCart, setGoToCart] = useState(false);
+
+const onAdd = (quantity) => {
+  setGoToCart(true);
+  addProduct(doc, quantity)
+}
+  
+  useEffect(()=>{
+    
+    const itemRef = doc(db, "items",id)
+    //traemos el documento
+    getDoc(itemRef )
+    .then((result)=>{
+      setProducto({
+        id:result.id,
+        ...result.data()
+      })
+    })
+    .catch((error)=> console.log(error))
+    .finally(()=> setLoading(false))
+  }, [id])
+
+
+
+  if (!producto) {
+    return <Navigate to="/404" />;
+  }
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+
+  return (
+    <div className={styles.border}>
+    <div className={styles.container}>
+      <h3>{producto.title}</h3>
+      <img src={producto.image} alt={producto.title} width="400" />
+      <p className={styles.description}>{producto.description}</p>
+      <h4>${producto.price}</h4>
+  {
+    goToCart 
+    ? <Link to="/cart"> <h3 className={styles.irCarrito}> Ir al carrito </h3></Link>
+    : <ItemCount initial={1} stock={10} onAdd={onAdd}/>
+  }
+    </div>
+    </div>
+  );
+};
+
+export default CardDetail;
